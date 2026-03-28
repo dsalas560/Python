d66cc8897607e592ecf583dd1ffd47c06eaeed09
Daniel Salas <47338606+dsalas560@users.noreply.github.com>
2026-03-28 09:36:55 -0700

    Initialize main game loop in Main.py

---
+++ Dungeon Crawler/Main.py
@@ -0,0 +1,17 @@
+import pygame
+from settings import *
+from game import Game
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption(TITLE)
+    clock = pygame.time.Clock()
+
+    game = Game(screen, clock)
+    game.run()
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
